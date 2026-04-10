e2a1f447428cc47b19a1dedc7f619fcb022e4b77
Serdar Gülec <gulec2000@gmail.com>
2026-04-10 16:40:24 +0200

    edit framer-motion

--- frontend/src/components/SkillsBento.js
+++ frontend/src/components/SkillsBento.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 import { useLang } from "../context/LanguageContext";
 import { 
   Globe, 
