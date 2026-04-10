ccd56439f2129bb016b684f63cc4c2608295e5aa
Serdar Gülec <gulec2000@gmail.com>
2026-04-10 16:32:34 +0200

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import SkillsBento from "./components/SkillsBento";
 import Blog from "./components/Blog";
-import Contact from "../Contact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
